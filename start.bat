8899d407794a659d092529d491c0d1c6fc4206c6
Gamal Shehata <s-gamal.shehata@zewailcity.edu.eg>
2026-07-10 12:05:04 +0300

    Hide terminal window on launch

--- start.bat
+++ start.bat
@@ -1,25 +1,19 @@
 @echo off
+if not "%1"=="--hidden" (
+    powershell -WindowStyle Hidden -NoProfile -Command "Start-Process cmd -ArgumentList '/c \"\"%~f0\" --hidden\"' -WindowStyle Hidden"
+    exit /b
+)
+
 cd /d "%~dp0"
 
 if not exist "node_modules\" (
-    echo Installing dependencies...
     call npm install
-    if %errorlevel% neq 0 (
-        echo Failed to install dependencies.
-        pause
-        exit /b %errorlevel%
-    )
+    if %errorlevel% neq 0 exit /b %errorlevel%
 )
 
 if not exist "dist\index.html" (
-    echo Building frontend...
     call npm run build
-    if %errorlevel% neq 0 (
-        echo Failed to build frontend.
-        pause
-        exit /b %errorlevel%
-    )
+    if %errorlevel% neq 0 exit /b %errorlevel%
 )
 
-echo Starting Focus Companion...
 start "" /B npm start >nul 2>&1
